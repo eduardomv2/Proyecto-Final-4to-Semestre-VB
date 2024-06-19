d086d09d3979318c37fc241f7433bf2a582ce592
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-06-19 15:14:41 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto Final 4to Semestre VB/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(12, 39)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(1402, 474)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(37, 576)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(109, 31)
+        Button1.TabIndex = 1
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(148, 576)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(109, 31)
+        Button2.TabIndex = 2
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(257, 576)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(109, 31)
+        Button3.TabIndex = 3
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(372, 576)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(109, 31)
+        Button4.TabIndex = 4
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(929, 576)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(109, 31)
+        Button5.TabIndex = 8
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(800, 576)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(109, 31)
+        Button6.TabIndex = 7
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(676, 576)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(109, 31)
+        Button7.TabIndex = 6
+        Button7.Text = "Button7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(558, 576)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(109, 31)
+        Button8.TabIndex = 5
+        Button8.Text = "Button8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(1216, 576)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 23)
+        Button9.TabIndex = 9
+        Button9.Text = "Button9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1426, 650)
+        Controls.Add(Button9)
+        Controls.Add(Button5)
+        Controls.Add(Button6)
+        Controls.Add(Button7)
+        Controls.Add(Button8)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+
+End Class
---
+++ Proyecto Final 4to Semestre VB/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.Configuration
+Imports System.IO
+Imports Newtonsoft.Json
+Imports YamlDotNet.Serialization
+Imports YamlDotNet.Serialization.NamingConventions
+
+Namespace Proyecto_Final_4to_Semestre
+    Partial Public Class Form1
+        Inherits Form
+
+        Private connectionString As String = ConfigurationManager.ConnectionStrings("MyConnectionString").ConnectionString
+        Private dataTable As DataTable
+        Private bindingSource As BindingSource
+        Private dataAdapter As SqlDataAdapter
+
+        Public Sub New()
+            InitializeComponent()
+            InitializeDataGridView()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            DataGridView.RowsDefaultCellStyle.ForeColor = Color.Black
+            DataGridView.DefaultCellStyle.BackColor = Color.FromArgb(220, 220, 220)
+            DataGridView.DefaultCellStyle.ForeColor = Color.Black
+            DataGridView.DefaultCellStyle.SelectionBackColor = Color.FromArgb(173, 216, 230)
+            DataGridView.DefaultCellStyle.SelectionForeColor = Color.Black
+
+            LoadData()
+        End Sub
+
+        Private Sub InitializeDataGridView()
+            bindingSource = New BindingSource()
+            DataGridView.DataSource = bindingSource
+        End Sub
+    End Class
+End Namespace
